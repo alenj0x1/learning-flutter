f5742fbc657ddff6f1a29d1e2aeee34b938d35d7
alenj0x1 <140274341+alenj0x1@users.noreply.github.com>
2024-12-31 11:03:59 -0500

    feat: added 01_hello_world and 02_variables

---
+++ 01-dart-intro/01_hello_world.dart
@@ -0,0 +1,9 @@
+void main() {
+  // var myName = 'alenj0x1';
+  // late final myName;
+  // String myName = 'alenj0x1';
+  const myName = 'alenj0x1';
+
+  print('hi $myName');
+  print('hi ${myName.toUpperCase()}');
+}

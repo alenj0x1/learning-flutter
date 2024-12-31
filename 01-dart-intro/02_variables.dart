e25c951c2c73eea64aca5ca024fb36b4b09cb499
alenj0x1 <140274341+alenj0x1@users.noreply.github.com>
2024-12-31 12:03:54 -0500

    feat: added dynamic

--- 01-dart-intro/02_variables.dart
+++ 01-dart-intro/02_variables.dart
@@ -5,6 +5,14 @@ void main() {
   final List<String> abilities = ['impostor'];
   final sprites = <String>['ditto/front.png', 'ditto/back.png'];
 
+  // dynamic = null
+  dynamic errorMessage = 'hi';
+  errorMessage = 1;
+  errorMessage = () => true;
+  errorMessage += 1;
+
+  print(errorMessage);
+
   print("""
     $pokemon
     $hp

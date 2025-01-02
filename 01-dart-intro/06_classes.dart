fd82df4c2f6574fc3ca2ae4487f858fc62537773
alenj0x1 <140274341+alenj0x1@users.noreply.github.com>
2025-01-01 23:24:51 -0500

    feat: added 07_name_parameters

--- 01-dart-intro/06_classes.dart
+++ 01-dart-intro/06_classes.dart
@@ -3,6 +3,11 @@ void main() {
 
   print(wolverine.toString());
   print(wolverine.name);
+
+  final john =
+      User(name: 'john', lastName: 'doe', password: 'john1234', age: 300);
+
+  print(john.toString());
 }
 
 class Hero {
@@ -18,5 +23,23 @@ class Hero {
   @override
   String toString() {
     return 'hero: $name';
+  }
+}
+
+class User {
+  String name;
+  String lastName;
+  String password;
+  int age;
+
+  User(
+      {required this.name,
+      required this.lastName,
+      required this.password,
+      required this.age});
+
+  @override
+  String toString() {
+    return 'user: $name-$lastName';
   }
 }

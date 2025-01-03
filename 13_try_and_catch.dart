2c3a14b1b25c1c042fbecc90a21fe1b275c3a257
alenj0x1 <140274341+alenj0x1@users.noreply.github.com>
2025-01-02 20:34:59 -0500

    feat: added 13_try_and_catch

--- 13_try_and_catch.dart
+++ 13_try_and_catch.dart
@@ -11,12 +11,25 @@ void main() async {
     print('end try catch');
   }
 
+  // specified exceptions
+  try {
+    final value = await httpGet('https://takataka.com');
+    print(value);
+  } on Exception catch (err) {
+    print('a exception: $err');
+  } catch (err) {
+    print('error: $err');
+  } finally {
+    print('end try catch');
+  }
+
   print('ending');
 }
 
 Future<String> httpGet(String url) async {
   await Future.delayed(Duration(seconds: 4));
 
-  throw 'a ocurred error';
+  throw Exception('taka');
+  //throw 'a ocurred error';
   // return 'hello world';
 }

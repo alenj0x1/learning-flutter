e197dc5f26232f44e9fc4fc3ad6b9acee4587a6b
alenj0x1 <140274341+alenj0x1@users.noreply.github.com>
2025-01-02 20:22:54 -0500

    feat: added 11_futures

---
+++ 01-dart-intro/11_futures.dart
@@ -0,0 +1,19 @@
+void main() {
+  print('starting');
+
+  httpGet('https://takataka.com').then((value) {
+    print(value);
+  }).catchError((err) {
+    print('error: $err');
+  });
+
+  print('ending');
+}
+
+Future<String> httpGet(String url) {
+  return Future.delayed(const Duration(seconds: 4), () {
+    throw Exception('a ocurred error');
+
+    // return 'hello world';
+  });
+}

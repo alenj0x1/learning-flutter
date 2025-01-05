ca5495cd89fc750d2032b792d86bb3b71b6b0c2d
alenj0x1 <140274341+alenj0x1@users.noreply.github.com>
2025-01-05 15:13:52 -0500

    feat: implemented floatingActionButton

--- hello_world_app/lib/presentation/screens/counter/counter_screen.dart
+++ hello_world_app/lib/presentation/screens/counter/counter_screen.dart
@@ -5,9 +5,21 @@ class CounterScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Counter Screen'),
+    return Scaffold(
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('10',
+                style: TextStyle(fontSize: 160, fontWeight: FontWeight.w100)),
+            Text('Clicks',
+                style: TextStyle(fontSize: 160, fontWeight: FontWeight.w100))
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.plus_one),
       ),
     );
   }

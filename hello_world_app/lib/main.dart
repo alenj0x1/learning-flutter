7c3319dff0a8846b56351cad8a4607f7204f1335
alenj0x1 <140274341+alenj0x1@users.noreply.github.com>
2025-01-05 14:53:19 -0500

    feat: added counter screen

--- hello_world_app/lib/main.dart
+++ hello_world_app/lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world_app/presentation/screens/counter/counter_screen.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Center(child: Text('hello world')));
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: CounterScreen(),
+    );
   }
 }

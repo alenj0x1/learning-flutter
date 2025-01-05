4a009f95ecead99773ee599a1bd6f588bc9addae
alenj0x1 <140274341+alenj0x1@users.noreply.github.com>
2025-01-05 15:55:02 -0500

    feat: implemented StatefulWidget and setState

--- hello_world_app/lib/presentation/screens/counter/counter_screen.dart
+++ hello_world_app/lib/presentation/screens/counter/counter_screen.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 
-class CounterScreen extends StatelessWidget {
+class CounterScreen extends StatefulWidget {
   const CounterScreen({super.key});
+
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int clickCounter = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -9,19 +16,23 @@ class CounterScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Counter Screen'),
       ),
-      body: const Center(
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('10',
+            Text('$clickCounter',
                 style: TextStyle(fontSize: 160, fontWeight: FontWeight.w100)),
             Text('Clicks',
-                style: TextStyle(fontSize: 160, fontWeight: FontWeight.w100))
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.w400))
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          setState(() {
+            clickCounter++;
+          });
+        },
         child: Icon(Icons.plus_one),
       ),
     );

21c901fa44ced5acc144482b46f2f3938c1ee449
alenj0x1 <140274341+alenj0x1@users.noreply.github.com>
2025-01-07 22:21:39 -0500

    feat: implemented ScrollController

--- yes_no_app/lib/presentation/providers/chat_provider.dart
+++ yes_no_app/lib/presentation/providers/chat_provider.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
+  final ScrollController chatScrollController = ScrollController();
+
   List<Message> messageList = [
     Message(text: 'hi', fromWho: FromWho.me),
     Message(text: 'how are you?', fromWho: FromWho.me),
@@ -9,9 +11,20 @@ class ChatProvider extends ChangeNotifier {
   ];
 
   Future<void> sendMessage(String text) async {
+    if (text.isEmpty) return;
+
     final newMessage = Message(text: text, fromWho: FromWho.me);
     messageList.add(newMessage);
 
     notifyListeners();
+    moveScrollToBottom();
+  }
+
+  void moveScrollToBottom() {
+    chatScrollController.animateTo(
+      chatScrollController.position.maxScrollExtent + 100,
+      duration: Duration(milliseconds: 100),
+      curve: Curves.bounceInOut,
+    );
   }
 }
